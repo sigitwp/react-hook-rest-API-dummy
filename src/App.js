0a6ff32f6c2bb496ee60b247447041c8b7ed10a1
sigit <sigit.setiaji@vizitrip.com>
2020-06-24 13:10:09 +0700

    update App.js implement useEffect & useState using dummy online rest API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
+  const [post, setPost] = useState({});
+  const [postId, setPostId] = useState(1);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      setIsFetching(true);
+      console.log("postId: ", postId);
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${postId}`
+      );
+      console.log("res: ", res);
+      const post = await res.json();
+      console.log("post: ", post);
+      setPost(post);
+      setIsFetching(false);
+    };
+    fetchPost();
+  }, [postId]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>React Hook - Dummy Online Rest API</h1>
+      <Button className="btn btn-primary btn-box" onClick={e => setPostId(postId + 1)}>Next Post</Button>
+      {isFetching && <h1>Loading...</h1>}
+      {!isFetching && (
+        <div className="card">
+          <div className="card-header">
+            <p>Post ID: {postId}</p>
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">{post.title}</h5>
+            <p className="card-text">{post.body}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
